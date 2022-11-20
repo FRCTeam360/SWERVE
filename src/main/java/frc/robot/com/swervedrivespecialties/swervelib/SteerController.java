3b5ef90e33b394905389a13bdec1b502906c7d73
juliashimshock <24shimshockjulia@bprep.org>
2022-11-20 15:06:25 -0800

    starting to work maybe?

---
+++ src/main/java/frc/robot/com/swervedrivespecialties/swervelib/SteerController.java
@@ -0,0 +1,9 @@
+package frc.robot.com.swervedrivespecialties.swervelib;
+
+public interface SteerController {
+    double getReferenceAngle();
+
+    void setReferenceAngle(double referenceAngleRadians);
+
+    double getStateAngle();
+}
